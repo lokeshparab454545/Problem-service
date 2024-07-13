cd53130636c4f1cb699452bed4400f43b934c0c0
Lokesh Parab <lokeshp@byteridge.com>
2024-07-13 16:44:57 +0530

    migrated to http-status-codes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,16 +1,38 @@
+const { StatusCodes } = require("http-status-codes");
+
 function pingProblemController(req, res) {
   return res.json({ message: "Problem controller is up" });
 }
 
-function addProblem(req, res) {}
+function addProblem(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not Implemented",
+  });
+}
 
-function getProblem(req, res) {}
+function getProblem(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not Implemented",
+  });
+}
 
-function getProblems(req, res) {}
+function getProblems(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not Implemented",
+  });
+}
 
-function deleteProblems(req, res) {}
+function deleteProblems(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not Implemented",
+  });
+}
 
-function updateProblems(req, res) {}
+function updateProblems(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not Implemented",
+  });
+}
 
 module.exports = {
   pingProblemController,
