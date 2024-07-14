c3b8709b98792dc5d6dcae16ab9aba60209840b7
Lokesh Parab <lokeshp@byteridge.com>
2024-07-14 08:47:44 +0530

    Added mongoose db config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/server.config");
 const apiRouter = require("./routes");
 const errorHandler = require("./utils/errorHandling");
+const connectToDB = require("./config/db.config");
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,6 +17,8 @@ app.get("/ping", (req, res) => {
 });
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server started a PORT: ${PORT}`);
+  await connectToDB();
+  console.log("Database successfully connected");
 });
